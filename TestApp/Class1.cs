9b33814e9ebc8e0ec50a252625e0a87c4e0a82c1
zaticpetru <zatic.petru@outlook.com>
2022-11-18 14:29:39 +0200

    Remove comments form select assignment

--- TestApp/Class1.cs
+++ TestApp/Class1.cs
@@ -6,14 +6,11 @@ public class Class1
 	{
 		int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
 
-        // 2. Query creation.
-        // numQuery is an IEnumerable<int>
         var numQuery =
             from num in numbers
             where (num % 2) == 0
             select num;
 
-        // 3. Query execution.
         foreach (int num in numQuery)
         {
             Console.Write("{0,1} ", num);
